30e6d4b15495722f1502d7b458977d428de3e929
eleanorbarnum1 <78886883+eleanorbarnum1@users.noreply.github.com>
2024-01-28 15:16:13 -0700

    Uploading files individually

---
+++ menu.js
@@ -0,0 +1,11 @@
+function toggleNav() {
+    var updateElement = document.getElementById("menu-icon");
+ //toggle adds a class if it's not there or removes it if it is.
+    updateElement.classList.toggle("open");
+
+    var updateElement = document.getElementById("nav");
+    updateElement.classList.toggle("open");
+    
+    
+} 
+
